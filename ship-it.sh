c9529a8c80691d0a712ff7025248dd403e86fd67
Anne LoVerso <anne.loverso@gmail.com>
2020-02-17 01:14:00 -0500

    Add run-once npm test script
    Also, don't ship when there are uncommitted changes
    
    Signed-off-by: George Gelashvili <walrusfruitcake@users.noreply.github.com>

--- ship-it.sh
+++ ship-it.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+set -e
 
-npm run test:once && git push
+changed_files=$(git status --porcelain | wc -l)
+if [ $changed_files -ne 0 ]; then
+  git status
+  echo
+  echo "^^^YOU GOT SOME UNCOMMITTED CHANGED IN 'ERE"
+  echo
+  exit 1
+fi
+
+npm run test:once
+git push
